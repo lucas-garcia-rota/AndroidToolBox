a9466eedd1ecf6e2d6196aee850de6c22e34cc1f
Lucas GARCIA--ROTA <lucas.garcia-rota@isen.yncrea.fr>
2020-01-21 17:32:52 +0100

    Calendrier amélioré et affiché sur un TextView plutôt qu'un Input

--- app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/FormActivity.kt
+++ app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/FormActivity.kt
@@ -3,9 +3,12 @@ package fr.isen.lucasgarciarota.androidtoolbox
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_form.*
+import java.text.SimpleDateFormat
 import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class FormActivity : AppCompatActivity() {
 
@@ -13,13 +16,27 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
+        val textView: TextView = findViewById(R.id.dateText)
+        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textView.text = sdf.format(cal.time)
+
+        }
+
         dateButton.setOnClickListener {
-            val now = Calendar.getInstance()
-            val datePicker = DatePickerDialog(this,DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Year : " + year + "\nMonth : " + month + "\nDay : " + dayOfMonth, Toast.LENGTH_SHORT).show()
-            },
-                now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH))
-            datePicker.show()
+            DatePickerDialog(this, dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 }
