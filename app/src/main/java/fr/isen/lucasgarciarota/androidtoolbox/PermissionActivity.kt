ed1bc85c6897c319c8ac0d35c7e1de8c90410328
Lucas GARCIA--ROTA <lucas.garcia-rota@isen.yncrea.fr>
2020-01-27 13:58:40 +0100

    Choix entre prendre une photo ou une photo de la galerie

--- app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/PermissionActivity.kt
+++ app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/PermissionActivity.kt
@@ -1,6 +1,8 @@
 package fr.isen.lucasgarciarota.androidtoolbox
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -11,6 +13,7 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import androidx.core.graphics.set
 import kotlinx.android.synthetic.main.activity_permission.*
@@ -21,27 +24,72 @@ import java.util.*
 
 class PermissionActivity : AppCompatActivity() {
 
+    companion object{
+        //Permission code
+        private const val IMAGE_PICK_REQUEST = 1000
+        private const val CAMERA_PICK_REQUEST = 4444
+        private const val CONTACT_PICK_REQUEST = 1001
+
+        const val PERMISSION_CODE = 1002
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
 
-        val photoButton: ImageView = findViewById(R.id.photoButton)
-
         photoButton.setOnClickListener {
-            val intent: Intent = Intent(Intent.ACTION_PICK)
-            intent.setType("image/*")
-            startActivityForResult(intent, 1)
+            withItems()
         }
+    }
+
+    fun imageFromGallery(){
+        val intent: Intent = Intent(Intent.ACTION_PICK)
+        intent.setType("image/*")
+        startActivityForResult(intent, IMAGE_PICK_REQUEST)
+    }
+
+    fun imageFromCamera(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, CAMERA_PICK_REQUEST)
+            }
+        }
+    }
+
+    fun withItems() {
+
+        val items = arrayOf("Camera", "Galerie")
+        val builder = AlertDialog.Builder(this)
+        with(builder)
+        {
+            setTitle("Choisir : ")
+            setItems(items) { _, which ->
+                if(items[which] == "Camera"){
+                    imageFromCamera()
+                }
+                else{
+                    imageFromGallery()
+                }
+            }
+
+            setPositiveButton("Retour", backButtonClick)
+            show()
+        }
+    }
+
+    private val backButtonClick = { _: DialogInterface, _: Int ->
+        Toast.makeText(applicationContext, "Aucune photo choisie", Toast.LENGTH_SHORT).show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == 1) {
-            if(data != null){
-                val imageUri: Uri = data?.data
-                val selectedImage: Bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
-                photoButton.setImageBitmap(selectedImage)
-            }
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_REQUEST){
+            photoButton.setImageURI(data?.data)
+        }
+
+        else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA_PICK_REQUEST){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photoButton.setImageBitmap(imageBitmap)
         }
     }
 }
