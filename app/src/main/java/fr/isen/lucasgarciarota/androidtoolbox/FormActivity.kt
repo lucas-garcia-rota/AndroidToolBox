2fd8bce5f6547ec9c72e73e206759295db2ab394
Lucas GARCIA--ROTA <lucas.garcia-rota@isen.yncrea.fr>
2020-01-27 08:10:29 +0100

    Premiere tentative de toast

--- app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/FormActivity.kt
+++ app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/FormActivity.kt
@@ -1,20 +1,25 @@
 package fr.isen.lucasgarciarota.androidtoolbox
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_form.*
 import java.text.SimpleDateFormat
 import java.util.*
-import javax.xml.datatype.DatatypeConstants.MONTHS
+
 
 class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        val formInput: SharedPreferences = getSharedPreferences("FormInfo", Context.MODE_PRIVATE)
 
         val textView: TextView = findViewById(R.id.dateText)
         textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
@@ -38,5 +43,51 @@ class FormActivity : AppCompatActivity() {
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
         }
+
+        submitButton.setOnClickListener {
+            jsonForm(formInput)
+        }
+
+        informationsButton.setOnClickListener {
+            popUp(formInput, 21)
+        }
+    }
+
+    fun popUp(formInput: SharedPreferences, age: Int) {
+        var gson: Gson = Gson()
+
+        var json: String = formInput.getString("Profile", "").toString()
+        var user: User
+        if (formInput != null) {
+            user = gson.fromJson(json, User::class.java)
+
+
+            var alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+            alertDialogBuilder
+                .setTitle("Informations")
+                .setMessage("Nom :" + user.name + "\nPrenom:" + user.firstname + "\nDate de naissance:" + user.birthDate + "\nAge:21")
+
+
+            var alertDialog: AlertDialog = alertDialogBuilder.create()
+
+            alertDialog.show()
+
+            print(age)
+
+        }
+
+    }
+
+    fun jsonForm(formInput: SharedPreferences){
+        var json: Gson = Gson()
+        val user: User = User(this.nomText.text.toString(), this.prenomText.text.toString(), this.dateText.text.toString())
+        val editor: SharedPreferences.Editor = formInput.edit()
+        editor.putString("Personne", json.toJson(user))
+        editor.apply()
+
+        this.nomText.setText("")
+        this.prenomText.setText("")
+        this.dateText.setText("")
     }
 }
---
+++ app/src/main/java/fr/isen/lucasgarciarota/androidtoolbox/User.kt
@@ -0,0 +1,16 @@
+package fr.isen.lucasgarciarota.androidtoolbox
+
+class User {
+    var name: String = ""
+    var firstname: String = ""
+    var birthDate: String = ""
+
+    constructor(name: String, firstname: String, date: String)
+    {
+        this.name = name
+        this.firstname = firstname
+        this.birthDate = birthDate
+    }
+
+
+}
